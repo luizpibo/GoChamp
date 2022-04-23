b4c1b5eb765d03642c1dec916e23380604ab19f5
luizpibo <luizfernandolima_09@hotmail.com>
2022-04-22 23:26:25 -0300

    adicionando AOS

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const campeonatosDisponiveis = require("../mocks");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("home", { campeonatosDisponiveis: campeonatosDisponiveis });
 });
 
 module.exports = router;
